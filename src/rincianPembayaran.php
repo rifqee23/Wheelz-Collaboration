31aa536fa4bcc92d2f38f0d298f0d32fc0055e1b
JessenRamadeksaAllen <chenallen244@gmail.com>
2024-06-08 21:50:54 +0700

    konsultasi via whatsapp

--- src/rincianPembayaran.php
+++ src/rincianPembayaran.php
@@ -59,7 +59,8 @@
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
                         <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
-                    </svg></span>Konsultasi Via Whatsapp</button>
+                    </svg></span>
+                    <a href="https://wa.me/085838767982">Konsultasi Via Whatsapp</a></button>
             <button class="px-4 w-60 py-3 text-2xl font-semibold text-white bg-blue-500 rounded-2xl">Lihat Rincian</button>
         </div>
         <div class="absolute bottom-0 w-full -z-20">
